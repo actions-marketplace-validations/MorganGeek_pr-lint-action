bb3e674ff7a2c42b5a7f7e989fe9ad7474d1385c
Bryce-Carter <66975975+Bryce-Carter@users.noreply.github.com>
2020-10-07 12:31:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -103,9 +103,9 @@ function findFailedCommits(projects, commitsInPR, ignoreCase) {
 }
 
 function createProjectRegex(project) {
-  return new RegExp(project)
+  return new RegExp(project + '-_\\d*')
 }
 
 function createWrappedProjectRegex(project) {
-  return new RegExp(project)
+  return new RegExp('\\' + project + '-\\d*\\')
 }
